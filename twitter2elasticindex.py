2a43e268f4419506c53026f11fb804a5f0c40cfa
tsselvaraj <timo.selvaraj@gmail.com>
2019-09-27 12:23:24 -0400

    Create twitter2elasticindex.py

---
+++ twitter2elasticindex.py
@@ -0,0 +1,53 @@
+from TwitterSearch import *
+
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+from elasticsearch import Elasticsearch
+import json
+es = Elasticsearch()
+es.indices.create(index='twitter', ignore=400)
+
+tweet_dict = {}
+
+try:
+    tso = TwitterSearchOrder()  # create a TwitterSearchOrder object
+    tso.set_keywords(['basketball', 'football'])  # let's define all words we would like to have a look for
+    tso.set_language('en')  # we want to see English tweets only
+    tso.set_include_entities(True)  # and don't give us all those entity information
+
+
+    # Please create a Twitter app key here https://apps.twitter.com
+    ts = TwitterSearch(
+        consumer_key='hjAiR0bG54BrCfoYo3sYg',
+        consumer_secret='w73bFtBO02jswmjw3ln3jXqyZ9Bwwdslw6ZM1HIBKI',
+        access_token='45871162-XX6R7StzotBFAIxHGGZkZ49GtoxdXFXpkmHi24ZqS',
+        access_token_secret='gpOtpOaOrE9nUrfKu4OAIzqIIUsaRyboMqFDHWnKCRvan'
+    )
+
+    count = 1
+    # iterate through the tweets
+    for tweet in ts.search_tweets_iterable(tso):
+        print('@%s tweeted: %s' % (tweet['user']['screen_name'], tweet['text']))
+
+        blob = TextBlob(tweet['text'])
+        print (tweet['text'])
+        print (blob.sentiment)
+        tweet_dict[count] = [str(tweet['id']), tweet['user']['screen_name'], tweet['text'], blob.sentiment[0]]
+        print (tweet_dict[count])
+        #tweettext = tweet['text']
+        es.index("twitter", "tweet", id=count, body=tweet)
+        count = count+1
+
+
+    #print (tweet_dict)
+    #df = pd.DataFrame.from_dict(tweet_dict, orient='index')
+    #df.rename(columns={0: 'TweetID'}, inplace=True)
+    #df.rename(columns={1: 'User'}, inplace=True)
+    #df.rename(columns={2: 'Tweet'}, inplace=True)
+    #df.rename(columns={3: 'Sentiment'}, inplace=True)
+
+    #print (df)
+
+
+except TwitterSearchException as e:  # take care of all those errors
+    print(e)
